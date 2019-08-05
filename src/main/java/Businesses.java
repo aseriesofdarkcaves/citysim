f34054b493deb93f1091b519da2d17a099e88b65
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2019-08-05 22:05:42 +0200

    Playing with a new idea of using these classes as a composite in City at a later stage

---
+++ src/main/java/Businesses.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Businesses {
+
+    private List<Business> businesses = new ArrayList<>();
+
+    Businesses() {
+        // TODO implementation
+    }
+}
---
+++ src/main/java/Tiles.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Tiles {
+
+    private Tile[][] tiles;
+    private static HashMap<TileType, Tile> instancedTiles = new HashMap<>();
+
+    Tiles() {
+        // TODO implementation
+    }
+}
