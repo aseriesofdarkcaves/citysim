b32aec3da8f6b0c669bc37cb9ebb8589a7779e17
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2020-02-19 15:42:28 +0100

    refactor of main to use event dispatch thread - according to Swing best practices

--- src/main/java/com/asodc/citysim/mvc/CitySim.java
+++ src/main/java/com/asodc/citysim/mvc/CitySim.java
@@ -1,11 +1,18 @@
 package com.asodc.citysim.mvc;
 
+import javax.swing.*;
+
 public class CitySim {
     public static void main(String... args) {
-        initCitySim();
+        // use event dispatch thread
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                startCitySim();
+            }
+        });
     }
 
-    private static void initCitySim() {
+    private static void startCitySim() {
         // passive MVC model - controller updates both model and view when notified
         CityModel model = new CityModel();
         CityView view = new CityView();
